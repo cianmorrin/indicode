a1cf9652eb04bb37015bec54dc3fe4ecd83cd9cd
Cian Morrin <c17718445@mytudublin.ie>
2021-02-08 19:37:49 +0000

    radio buttons and state created

--- indicode/frontend/src/components/indicode/Questionnaire.js
+++ indicode/frontend/src/components/indicode/Questionnaire.js
@@ -7,23 +7,66 @@ export class Questionnaire extends Component {
   static propTypes = {
     questionnaire: PropTypes.array.isRequired,
     getQuestionnaire: PropTypes.func.isRequired,
+    questionnaireResults: PropTypes.array,
   };
+
+  state = {};
 
   componentDidMount() {
     this.props.getQuestionnaire();
   }
 
+  onChange = (e) => {
+    const { name, value } = e.target;
+
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state);
+    // const { name, email, message } = this.state;
+    // const lead = { name, email, message };
+    // this.props.addLead(lead);
+    // this.setState({
+    //   name: "",
+    //   email: "",
+    //   message: "",
+    // });
+  };
+
   render() {
     return (
       <Fragment>
         <h2>Questionnaire</h2>
-        {this.props.questionnaire.map((questionnaire) => (
-          <div key={questionnaire.id}>
-            <div>{questionnaire.question}</div>
-            <div>{questionnaire.option_A}</div>
-            <div>{questionnaire.option_B}</div>
+        <form onSubmit={this.onSubmit}>
+          {this.props.questionnaire.map((questionnaire) => (
+            <div key={questionnaire.id}>
+              <h4>{questionnaire.question}</h4>
+              <input
+                type="radio"
+                value="option_A"
+                name={questionnaire.id}
+                onChange={this.onChange}
+              />{" "}
+              {questionnaire.option_A}
+              <input
+                type="radio"
+                value="option_B"
+                name={questionnaire.id}
+                onChange={this.onChange}
+              />{" "}
+              {questionnaire.option_B}
+            </div>
+          ))}
+          <div className="form-group">
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
           </div>
-        ))}
+        </form>
       </Fragment>
     );
   }
