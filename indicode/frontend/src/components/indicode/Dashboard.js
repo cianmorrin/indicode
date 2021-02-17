e8535b1f2cebdabd92ed0db10cf3fae8aa98e0b4
Cian Morrin <c17718445@mytudublin.ie>
2021-02-17 20:30:31 +0000

    dashboard started

--- indicode/frontend/src/components/indicode/Dashboard.js
+++ indicode/frontend/src/components/indicode/Dashboard.js
@@ -33,7 +33,56 @@ export class Dashboard extends Component {
         <LSResults learningStyleResults={this.props.learningStyleResults} />
       );
     }
-    return <Fragment>{comp}</Fragment>;
+    return (
+      <Fragment>
+        <div className="row mb-2">
+          <div className="col-md-8">
+            <div className="card border-primary mb-3 main-card">
+              <h2 className="card-header main-panel-header">
+                Welcome to IndiCode
+              </h2>
+              <p className="card-body">
+                IndiCode is desgined with you in mind. We want to teach you
+                Python but in the way you want to be taught! Fill out out the
+                questionnaire to determine your learning style and start the
+                Python learning
+              </p>
+              <hr className="my-4"></hr>
+              <p className="card-body">
+                Once your style is determined we can begin the process
+              </p>
+              <p className="card-body">
+                <a className="btn btn-primary btn-med" href="#" role="button">
+                  Learn more
+                </a>
+              </p>
+            </div>
+            <div className="row mb-2">
+              <div className="col-md-5 bottom-panels border ">
+                <h4 className="card-title">Continue Learning</h4>
+                <p className="card-text">Python 3</p>
+              </div>
+              <div className="col-md-1"></div>
+              <div className="col-md-5 bottom-panels border">
+                <h4 className="card-title">Your stats</h4>
+                <p className="card-text">Check out your progress</p>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3 dashbaord-right-side">
+            <div className="dashboard-side-panels border border-primary">
+              <h1>Row 1</h1>
+            </div>
+            <div className="dashboard-side-panels border border-primary">
+              <h1>Row 2</h1>
+            </div>
+            <div className=" dashboard-side-panels border border-primary">
+              <h1>Row 3</h1>
+            </div>
+          </div>
+        </div>
+      </Fragment>
+    );
   }
 }
 
