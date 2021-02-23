e88b81cf46ea170d91550f9b87902ce69f553fc2
Cian Morrin <c17718445@mytudublin.ie>
2021-02-23 00:29:13 +0000

    first lesson started

---
+++ indicode/frontend/src/components/indicode/Interpreter.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Interpreter() {
+  return (
+    <div>
+      <iframe
+        className="interpreter"
+        src="https://trinket.io/embed/python/3d8d7ce66b"
+        frameBorder="0"
+        allowFullScreen
+      ></iframe>
+    </div>
+  );
+}
--- indicode/frontend/src/components/indicode/Learning.js
+++ indicode/frontend/src/components/indicode/Learning.js
@@ -1,13 +1,20 @@
 import React, { Component, Fragment } from "react";
 import { connect } from "react-redux";
+import { Link, Redirect } from "react-router-dom";
 import PropTypes from "prop-types";
 import { getLearning } from "../../actions/learning";
 import { getLearningStyleResults } from "../../actions/questionnaire";
-import Var_A_R_Code from "../images/Var_A_R_Code.png";
+import Interpreter from "./Interpreter";
+import LearningContent from "./LearningContent";
+import LearningPagination from "./LearningPagination";
+import Mod_1_1 from "../images/mod_1_1.png";
 
 export class Learning extends Component {
   state = {
     started: false,
+    currentPage: 1,
+    lessonsPerPage: 1,
+    finishedModule: false,
   };
 
   static propTypes = {
@@ -29,112 +36,355 @@ export class Learning extends Component {
     }));
   };
 
+  onFinish = (e) => {
+    e.preventDefault();
+    this.setState(() => ({
+      finishedModule: true,
+    }));
+  };
+
+  setCurrentPage = (pageNumber) => {
+    this.setState(() => ({
+      currentPage: pageNumber,
+    }));
+  };
+
   render() {
+    if (this.state.finishedModule) {
+      return <Redirect to="/quiz" />;
+    }
     const act_ref = this.props.learningStyleResults[0].act_or_ref;
     const sen_int = this.props.learningStyleResults[0].sen_or_int;
     const vis_verb = this.props.learningStyleResults[0].vis_or_verb;
     const seq_glob = this.props.learningStyleResults[0].seq_or_glob;
 
-    let title, submodule, act_ref_content, sen_int_sentence, intro;
+    let title, submodule, intro;
+    let learning_content_comp, header_comp;
 
     if (this.props.learning.length > 0) {
-      title = this.props.learning[0].module;
-      submodule = this.props.learning[0].sub_module;
-      intro = this.props.learning[0].intro;
-      switch (act_ref) {
-        case "act11":
-        case "act9":
-          act_ref_content = (
+      switch (this.state.currentPage) {
+        case 1:
+          title = this.props.learning[0].module;
+          submodule = this.props.learning[0].sub_module;
+          intro = this.props.learning[0].intro;
+          header_comp = (
             <Fragment>
-              <p>{this.props.learning[0].active_3_1}</p>
-              <img src={Var_A_R_Code} />
-              <p>{this.props.learning[0].active_3_2}</p>
+              <h1>{title}</h1>
+              <h5>{submodule}</h5>
+              <hr className="my-4"></hr>
+              <p>{intro}</p>
             </Fragment>
           );
+          switch (act_ref) {
+            case "act11":
+            case "act9":
+              learning_content_comp = (
+                <div>
+                  {header_comp}
+                  <Fragment>
+                    <p>{this.props.learning[0].active_3_1}</p>
+                    <div>
+                      {" "}
+                      <img src={Mod_1_1} />
+                    </div>
+                    <p>{this.props.learning[0].active_3_2}</p>
+                  </Fragment>
+                </div>
+              );
+              break;
+            case "act7":
+            case "act5":
+              learning_content_comp = (
+                <div>
+                  {header_comp}
+                  <Fragment>
+                    <p>{this.props.learning[0].active_2_1}</p>
+                    <div>
+                      {" "}
+                      <img src={Mod_1_1} />
+                    </div>
+                    <p>{this.props.learning[0].active_2_2}</p>
+                  </Fragment>
+                </div>
+              );
+              break;
+            case "act3":
+            case "act1":
+              learning_content_comp = (
+                <div>
+                  {header_comp}
+                  <Fragment>
+                    <p>{this.props.learning[0].active_1_1}</p>
+                    <p>{this.props.learning[0].active_1_2}</p>
+                    <div>
+                      {" "}
+                      <img src={Mod_1_1} />
+                    </div>
+                  </Fragment>
+                </div>
+              );
+              break;
+            case "ref11":
+            case "ref9":
+              learning_content_comp = (
+                <div>
+                  {header_comp}
+                  <Fragment>
+                    <p>{this.props.learning[0].reflective_3_1}</p>
+                    <p>{this.props.learning[0].reflective_3_2}</p>
+                    <div>
+                      {" "}
+                      <img src={Mod_1_1} />
+                    </div>
+                  </Fragment>
+                </div>
+              );
+              break;
+            case "ref7":
+            case "ref5":
+              learning_content_comp = (
+                <div>
+                  {header_comp}
+                  <Fragment>
+                    <p>{this.props.learning[0].reflective_2_1}</p>
+                    <p>{this.props.learning[0].reflective_2_2}</p>
+                    <div>
+                      {" "}
+                      <img src={Mod_1_1} />
+                    </div>
+                  </Fragment>
+                </div>
+              );
+              break;
+            case "ref3":
+            case "ref1":
+              learning_content_comp = (
+                <div>
+                  {header_comp}
+                  <Fragment>
+                    <p>{this.props.learning[0].reflective_1_1}</p>
+                    <p>{this.props.learning[0].reflective_1_2}</p>
+                    <div>
+                      {" "}
+                      <img src={Mod_1_1} />
+                    </div>
+                  </Fragment>
+                </div>
+              );
+              break;
+            default:
+              learning_content_comp = "No data found";
+              break;
+          }
           break;
-        case "act7":
-        case "act5":
-          act_ref_content = (
+        case 2:
+          title = this.props.learning[1].module;
+          submodule = this.props.learning[1].sub_module;
+          intro = this.props.learning[1].intro;
+          header_comp = (
             <Fragment>
-              <p>{this.props.learning[0].active_2_1}</p>
-              <img src={Var_A_R_Code} />
-              <p>{this.props.learning[0].active_2_2}</p>
+              <h1>{title}</h1>
+              <h5>{submodule}</h5>
+              <hr className="my-4"></hr>
+              <p>{intro}</p>
             </Fragment>
           );
+          switch (vis_verb) {
+            case "vis11":
+            case "vis9":
+              learning_content_comp = (
+                <div>
+                  {header_comp}
+                  <Fragment>
+                    <p>{this.props.learning[1].visual_3_1}</p>
+                    <p>{this.props.learning[1].visual_3_2}</p>
+                    <div>
+                      {" "}
+                      <img src={Mod_1_1} />
+                    </div>
+                  </Fragment>
+                </div>
+              );
+              break;
+            case "vis7":
+            case "vis5":
+              learning_content_comp = (
+                <div>
+                  {header_comp}
+                  <Fragment>
+                    <p>{this.props.learning[1].visual_2_1}</p>
+                    <p>{this.props.learning[1].visual_2_2}</p>
+                    <div>
+                      {" "}
+                      <img src={Mod_1_1} />
+                    </div>
+                  </Fragment>
+                </div>
+              );
+              break;
+            case "vis3":
+            case "vis1":
+              learning_content_comp = (
+                <div>
+                  {header_comp}
+                  <Fragment>
+                    <p>{this.props.learning[1].visual_1_1}</p>
+                    <p>{this.props.learning[1].visual_1_2}</p>
+                    <div>
+                      {" "}
+                      <img src={Mod_1_1} />
+                    </div>
+                  </Fragment>
+                </div>
+              );
+              break;
+            case "verb11":
+            case "verb9":
+              learning_content_comp = (
+                <div>
+                  {header_comp}
+                  <Fragment>
+                    <p>{this.props.learning[1].verbal_3_1}</p>
+                    <p>{this.props.learning[1].verbal_3_2}</p>
+                    <p>{this.props.learning[1].verbal_3_3}</p>
+                  </Fragment>
+                </div>
+              );
+              break;
+            case "verb7":
+            case "verb5":
+              learning_content_comp = (
+                <div>
+                  {header_comp}
+                  <Fragment>
+                    <p>{this.props.learning[1].verbal_2_1}</p>
+                    <p>{this.props.learning[1].verbal_2_2}</p>
+                    <p>{this.props.learning[1].verbal_2_3}</p>
+                  </Fragment>
+                </div>
+              );
+              break;
+            case "verb3":
+            case "verb1":
+              learning_content_comp = (
+                <div>
+                  {header_comp}
+                  <Fragment>
+                    <p>{this.props.learning[1].verbal_1_1}</p>
+                    <p>{this.props.learning[1].verbal_1_2}</p>
+                    <p>{this.props.learning[1].verbal_1_3}</p>
+                  </Fragment>
+                </div>
+              );
+              break;
+            default:
+              learning_content_comp = "No data found";
+              break;
+          }
           break;
-        case "act3":
-        case "act1":
-          act_ref_content = (
+        case 3:
+          title = this.props.learning[2].module;
+          submodule = this.props.learning[2].sub_module;
+          intro = this.props.learning[2].intro;
+          header_comp = (
             <Fragment>
-              <p>{this.props.learning[0].active_1_1}</p>
-              <p>{this.props.learning[0].active_1_2}</p>
-              <img src={Var_A_R_Code} />
+              <h1>{title}</h1>
+              <h5>{submodule}</h5>
+              <hr className="my-4"></hr>
+              <p>{intro}</p>
             </Fragment>
           );
-          break;
-        case "ref11":
-        case "ref9":
-          act_ref_content = (
-            <Fragment>
-              <p>{this.props.learning[0].reflective_3_1}</p>
-              <p>{this.props.learning[0].reflective_3_2}</p>
-              <img src={Var_A_R_Code} />
-            </Fragment>
-          );
-          break;
-        case "ref7":
-        case "ref5":
-          act_ref_content = (
-            <Fragment>
-              <p>{this.props.learning[0].reflective_2_1}</p>
-              <p>{this.props.learning[0].reflective_2_2}</p>
-              <img src={Var_A_R_Code} />
-            </Fragment>
-          );
-          break;
-        case "ref3":
-        case "ref1":
-          act_ref_content = (
-            <Fragment>
-              <p>{this.props.learning[0].reflective_1_1}</p>
-              <p>{this.props.learning[0].reflective_1_2}</p>
-              <img src={Var_A_R_Code} />
-            </Fragment>
-          );
+          switch (sen_int) {
+            case "sen11":
+            case "sen9":
+              learning_content_comp = (
+                <div>
+                  {header_comp}
+                  <Fragment>
+                    <p>{this.props.learning[2].sensing_3}</p>
+                  </Fragment>
+                </div>
+              );
+              break;
+            case "sen7":
+            case "sen5":
+              learning_content_comp = (
+                <div>
+                  {header_comp}
+                  <Fragment>
+                    <p>{this.props.learning[2].sensing_2}</p>
+                  </Fragment>
+                </div>
+              );
+              break;
+            case "sen3":
+            case "sen1":
+              learning_content_comp = (
+                <div>
+                  {header_comp}
+                  <Fragment>
+                    <p>{this.props.learning[2].sensing_1}</p>
+                  </Fragment>
+                </div>
+              );
+              break;
+            case "int11":
+            case "int9":
+              learning_content_comp = (
+                <div>
+                  {header_comp}
+                  <Fragment>
+                    <p>{this.props.learning[2].intuitive_3}</p>
+                  </Fragment>
+                </div>
+              );
+              break;
+            case "int7":
+            case "int5":
+              learning_content_comp = (
+                <div>
+                  {header_comp}
+                  <Fragment>
+                    <p>{this.props.learning[2].intuitive_2}</p>
+                  </Fragment>
+                </div>
+              );
+              break;
+            case "int3":
+            case "int1":
+              learning_content_comp = (
+                <div>
+                  {header_comp}
+                  <Fragment>
+                    <p>{this.props.learning[2].intuitive_2}</p>
+                  </Fragment>
+                </div>
+              );
+              break;
+            default:
+              learning_content_comp = "No data found";
+              break;
+          }
           break;
         default:
-          act_ref_content = "No data found";
-          break;
+          learning_content_comp = "No data found";
       }
-      switch (sen_int) {
-        case "sen11":
-        case "sen9":
-          sen_int_sentence = this.props.learning[0].sensing_3;
-          break;
-        case "sen7":
-        case "sen5":
-          sen_int_sentence = this.props.learning[0].sensing_2;
-          break;
-        case "sen3":
-        case "sen1":
-          sen_int_sentence = this.props.learning[0].sensing_1;
-          break;
-        case "int11":
-        case "int9":
-          sen_int_sentence = this.props.learning[0].intuitive_3;
-          break;
-        case "int7":
-        case "int5":
-          sen_int_sentence = this.props.learning[0].intuitive_2;
-          break;
-        case "int3":
-        case "int1":
-          sen_int_sentence = this.props.learning[0].intuivite_1;
-          break;
-        default:
-          sen_int_sentence = "No data found";
-          break;
-      }
+    }
+
+    let finishComp = (
+      <div className="form-group">
+        Let's test your knowlege with the Module Quiz!
+        <div>
+          <button type="submit" className="btn btn-primary">
+            Finished
+          </button>
+        </div>
+      </div>
+    );
+
+    let showFinish = false;
+    if (this.state.currentPage == 3) {
+      showFinish = true;
     }
 
     return (
@@ -159,20 +409,19 @@ export class Learning extends Component {
             </div>
           </div>
           <div className={this.state.started ? "show-up" : "no-show"}>
-            <h1>{title}</h1>
-            <h5>{submodule}</h5>
-            <hr className="my-4"></hr>
-            <p>{intro}</p>
-            {act_ref_content}
+            <form onSubmit={this.onFinish}>
+              <LearningContent lesson={learning_content_comp} />
+              {showFinish ? finishComp : ""}
+            </form>
+            <LearningPagination
+              lessonsPerPage={this.state.lessonsPerPage}
+              totalLessons={3}
+              paginate={this.setCurrentPage}
+            />
           </div>
         </div>
         <div className="interpreter">
-          <iframe
-            className="interpreter"
-            src="https://trinket.io/embed/python/3d8d7ce66b"
-            frameBorder="0"
-            allowFullScreen
-          ></iframe>
+          <Interpreter />
         </div>
       </div>
     );
