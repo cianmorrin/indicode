5abff9c6b279a3ae7d0e52801b1c85ff99ea684a
Cian Morrin <c17718445@mytudublin.ie>
2021-03-15 13:49:47 +0000

    migrate db

--- indicode/indicode/settings.py
+++ indicode/indicode/settings.py
@@ -86,8 +86,11 @@ WSGI_APPLICATION = 'indicode.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'db_indi',
+        'USER': 'postgres',
+        'PASSWORD': 'indipass',
+        'HOST': 'localhost'
     }
 }
 
