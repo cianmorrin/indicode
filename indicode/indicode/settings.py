ba24f4015892bc93af8acf4e5e615d2b14e02f5a
Cian Morrin <c17718445@mytudublin.ie>
2021-03-15 17:02:14 +0000

    c

--- indicode/indicode/settings.py
+++ indicode/indicode/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'build/static')
+    os.path.join(BASE_DIR, 'frontend/static/frontend')
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
